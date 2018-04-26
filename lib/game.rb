c1015c591e78b57ef43f1c9d6b3c0e1476ab9456
vadim <david.junior1214@gmail.com>
2018-04-26 13:19:06 +0300

    Habar n-am de ce nu merge!
    Crapa chiar la primul apel move(0,0)

---
+++ lib/game.rb
@@ -0,0 +1,119 @@
+require 'set'
+require 'byebug'
+
+class Game
+
+	def initialize
+		@biarray = [['.','.','.'],['.','.','.'],['.','.','.']]
+		@role_to_move = "X moves"
+		@role_changer = 0
+	end
+	
+	def player_sign(mark, x, y) #
+		@role_to_move = "#{mark} moves"
+		if @biarray[x][y] != "."
+			@role_changer += 2
+			:invalid
+		else
+			@role_changer += 1
+			@biarray[x][y] = mark
+			:ok
+		end
+	end
+
+	def move(x,y)
+	  if x >= 0 && y >= 0 && x <= 2 && y <= 2
+			if state[:game_state] = :on
+				if @role_changer.even?
+				  player_sign("X", x, y)
+			  else
+		  	  player_sign("O", x, y)
+		  	end
+		  else
+		  	:invalid
+		  end
+	  else
+	    :invalid
+	  end
+	end
+
+	def same_value(array)
+		array.uniq.length == 1
+	end
+	
+	def inverse
+		(0..2).map { |index| @biarray.map { |line| line[index] }}
+	end
+
+	def main_diagonal_process
+		@main_diagonal = []
+		for i in 0..2
+			if @biarray[i][i] == 'X'
+				@main_diagonal << 'X'
+			elsif @biarray[i][i] == 'O'
+				@main_diagonal << 'O'
+			else
+				""
+			end
+		end
+		same_value(@main_diagonal) ? @role_to_move = "#{@main_diagonal[0]} won" : ""
+ 		same_value(@main_diagonal)
+	end
+
+	def second_diagonal_process
+		@second_diagonal = []
+		for i in 0..2
+			if @biarray[i][3-i-1] == 'X'
+				@second_diagonal << 'X'
+			elsif @biarray[i][3-i-1] == 'O'
+				@second_diagonal << 'O'
+			else
+				""
+			end
+		end
+		same_value(@second_diagonal) ? @role_to_move = "#{@second_diagonal[0]} won" : ""
+		same_value(@second_diagonal)
+	end
+
+	def lines_checker
+		if @biarray.any? { |line| ['X', 'O'].any? { |mark| line.all? { |val| val == mark } } }
+      @role_to_move = "#{@role_changer % 2 == 0 ? 'X' : 'O'} won"
+			true
+		elsif inverse.any? { |line| ['X', 'O'].any? { |mark| line.all? { |val| val == mark  } } }
+			@role_to_move = "#{@role_changer % 2 == 0 ? 'X' : 'O'} won"
+			true			
+		else
+			false
+		end
+	end
+		
+	def draw_checker
+		@biarray.map do |array| 
+			array.map do |e| 
+				if e == "."  
+					false 
+				else 
+					@role_to_move = "Draw"
+					true
+				end
+			end
+		end
+	end
+
+	def play_on #Return on if game is still on and over if game is over
+		if main_diagonal_process || second_diagonal_process || lines_checker || draw_checker
+			state[:game_state] = :over
+		else
+			state[:game_state] = :on
+		end
+	end
+	
+	def state
+		{
+			:board => @biarray,
+			:game_state => play_on,
+			:detail => @role_to_move
+		}
+	end
+
+end
---
+++ spec/game_spec.rb
@@ -0,0 +1,146 @@
+require 'spec_helper'
+require 'game'
+
+describe Game, "A simple TicTacToe game implementation" do
+  before :each do
+    @game = Game.new
+  end
+
+  describe "#new" do
+    it "returns a new Game object" do
+      @game.should be_an_instance_of Game
+    end
+  end
+
+  describe "#move" do
+    it "marks the provided position on the board with the character whose next turn it is to move" do
+       @game.move(0, 0).should eql :ok
+       board = @game.state[:board]
+       board[0][0].should eql 'X'
+
+       @game.move(2, 1).should eql :ok
+       board = @game.state[:board]
+       board[2][1].should eql 'O'
+    end
+
+    it "checks that the provided position doesn't fall outside the board limits before making the move" do
+      @game.move(0, -1).should eql :invalid
+      @game.move(2, 4).should eql :invalid
+      # etc
+    end
+
+    it "checks that the provided position isn't already taken before making the move" do
+      @game.move(0, 0).should eql :ok
+      @game.move(0, 0).should eql :invalid
+      # etc
+    end
+
+    it "checks that the game is still on before making the move" do
+      @game.move 0, 0
+      @game.move 1, 0
+      @game.move 0, 1
+      @game.move 1, 1
+      @game.move 0, 2
+      # at this point X has won
+      
+      @game.move(2, 2).should eql :invalid # an otherwise valid move...
+    end
+
+    it "takes turns correctly" do
+      @game.state[:detail].should eql 'X moves'
+
+      @game.move 0, 0
+      @game.state[:detail].should eql 'O moves'
+
+      @game.move 0, 1
+      @game.state[:detail].should eql 'X moves'
+    end
+  end
+
+  describe "#state" do
+    it "returns a hash containing 3 keys: board (array of 3 arrays with 3 elements), game_state and detail" do
+      state = @game.state
+      
+      state.should be_an_instance_of Hash
+
+      state[:board].should be_an_instance_of Array
+      state[:board].size.should eql 3
+      state[:board].map { |a| a.size }.should eql [3, 3, 3]
+
+      state.keys.should eql [:board, :game_state, :detail]
+    end
+
+    it "returns a { empty board, :on, 'X moves' } state for a new Game" do
+      state = @game.state
+
+      state[:board].should eql [['.','.','.'], ['.','.','.'], ['.','.','.']]
+      state[:game_state].should eql :on
+      state[:detail].should eql 'X moves'
+    end
+
+    it "returns a { non-empty board containing only '.','X' or 'O' elements, :on, 'X moves' or 'O moves' } state for a Game that had some moves made but it's still on" do
+      @game.move 0,0
+      @game.move 2,1
+      @game.move 1,2
+
+      state = @game.state
+
+      state[:board].flatten.all? { |c| ['.','X','O'].include? c }.should eql true
+      state[:game_state].should eql :on
+      state[:detail].should match /[X|O] moves/
+    end
+
+    it "returns a { non-empty board containing only 'X' and 'O' elements, :over, 'draw' } state for a Game that ended with a Draw" do
+      @game.move 0, 0    
+      @game.move 1, 0
+      @game.move 0, 1
+      @game.move 1, 1
+      @game.move 1, 2
+      @game.move 0, 2
+      @game.move 2, 0
+      @game.move 2, 2
+      @game.move 2, 1
+
+      state = @game.state
+
+      state[:board].flatten.all? { |c| ['X','O'].include? c }.should eql true
+      state[:game_state].should eql :over
+      state[:detail].should eql 'draw'
+    end
+
+    it "returns a { non-empty board, :over, 'X won' } state for a Game that ended with a victory for X" do
+      @game.move 0, 0
+      @game.move 1, 0
+      @game.move 0, 1
+      @game.move 1, 1
+      @game.move 0, 2
+
+      state = @game.state
+
+      state[:board].flatten.all? { |c| ['.','X','O'].include? c }.should eql true
+      state[:game_state].should eql :over
+      state[:detail].should eql 'X won'
+
+      state[:board].flatten.all? { |c| ['.','X','O'].include? c }.should eql true
+      state[:game_state].should eql :over
+      state[:detail].should eql 'X won'
+    end
+
+    it "returns a { non-empty board, :over, 'O won' } state for a Game that ended with a victory for O" do
+      @game.move 0, 0
+      @game.move 1, 0
+      @game.move 0, 1
+      @game.move 1, 1
+      @game.move 2, 2
+      @game.move 1, 2
+
+      state = @game.state
+
+      state[:board].flatten.all? { |c| ['.','X','O'].include? c }.should eql true
+      state[:game_state].should eql :over
+      state[:detail].should eql 'O won'
+    end
+
+    # TODO check that it correctly detects victories on lines, columns and diag's etc
+  end
+end
